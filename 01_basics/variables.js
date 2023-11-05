3c68578250eb654075727bd8b350c26cf0cdbaed
KshitizRepo <113644657+KshitizRepo@users.noreply.github.com>
2023-11-05 18:46:10 +0000

    variables in java script

---
+++ 01_basics/variables.js
@@ -0,0 +1,15 @@
+const accountId = 2222
+let accountEmail = "choudhary.kshitiz97@gmail.com"
+var accountPassword = "12345"
+let accountCity = "Modinagar"
+
+//accountId=4444
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity])
+
+/*
+we are not supposed to use var 
+because of issue in block scope and functional scope
+
+We will use let to declare the variables afterwards
+*/
