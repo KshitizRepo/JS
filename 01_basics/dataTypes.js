fc14bfc1c38907f63cd8b2efcd761b7e58a0f00e
KshitizRepo <113644657+KshitizRepo@users.noreply.github.com>
2023-11-06 17:10:46 +0000

    datatypes

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("hello "); // we are using node and not the browser
+
+console.log(3 +
+    3) // code readibility should be high
+
+console.log("Kshitiz")
+
+let name = "Kshitiz"
+let age = 24
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string =>""
+// boolean => true or false
+// null => standalone value and type
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof null)
